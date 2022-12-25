9a09e4f57d70dbbde44b3c316e0d5ea77c4e69f4
igostamikh <i.mikhin@gmail.com>
2022-12-25 18:07:00 +0200

    Lesson 76

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+
+  if (!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Введіть число!';
+  }
+});
