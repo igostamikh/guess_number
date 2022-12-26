549e7c59be7ed70bcbed931c40e80fafd7f79be7
igostamikh <69596045+igostamikh@users.noreply.github.com>
2022-12-26 14:41:55 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 "use strict";
 
-const secretNumber = Math.round(Math.random() * 20 + 1);
-let score = 20;
+let secretNumber;
+let score;
 let highscore = 0;
-
-document.querySelector(".question").textContent = secretNumber; //to delet
-document.querySelector(".score").textContent = score;
-document.querySelector(".number-input").value = "";
+//Функція ініциації нової гри
+function startNewGame() {
+  secretNumber = Math.round(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number-input").value = "";
+  document.querySelector(".check").disabled = false;
+  document.querySelector(".question").textContent = secretNumber; //to delet
+}
 //Функція змінює очки та дає підказки
 function checkNumber(str, key = true) {
   document.querySelector(".guess-message").textContent = str;
@@ -14,13 +19,15 @@ function checkNumber(str, key = true) {
     score--;
     document.querySelector(".score").textContent = score;
   } else {
+    document.querySelector(".check").disabled = true;
+    document.querySelector(".question").textContent = secretNumber;
     if (highscore < score) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
   }
 }
-
+//Обробка кнопки "Проверить"
 document.querySelector(".check").addEventListener("click", function () {
   const guessingNumber = Number(document.querySelector(".number-input").value);
 
@@ -29,19 +36,26 @@ document.querySelector(".check").addEventListener("click", function () {
       "Введите число от 1 до 20!";
   } else if (guessingNumber === secretNumber) {
     checkNumber("Победа!", false);
-  } else if (guessingNumber > secretNumber) {
+  } else if (guessingNumber > secretNumber && score > 1) {
     checkNumber("Меньше!");
-  } else if (guessingNumber < secretNumber) {
+  } else if (guessingNumber < secretNumber && score > 1) {
     checkNumber("Больше!");
-  }
-  
-  if (score === 0) {
+  } else {
     document.querySelector(".check").disabled = true;
-    document.querySelector(".guess-message").textContent = "Вы проиграли.";
+    checkNumber("Вы проиграли.");
   }
 });
-/*TODO: 1.Визначити число від 1 до 20
+//Обробка кнопки "Сначала"
+document.querySelector(".again").addEventListener("click", startNewGame);
+
+startNewGame();
+/*1.Визначити число від 1 до 20
 2.Порівняти з введенним:
  а) якщо замале вивести - "Більше!",
  б) якщо завелике вивести - "Менше",
  в) якщо співпало вивести - "Перемога!" */
+
+/*TODO: 1.При досягненні лічільником 0:
+а) заблокувати кнопку "Проверить!,
+б) вивести - "Проигрыш."
+2.При натисненні на кнопку "Сначала!" перезапустити всі значення до початкових.*/
