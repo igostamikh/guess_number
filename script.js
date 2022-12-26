1f24d026d6993ed39727030c3de1047a9f730edc
igostamikh <i.mikhin@gmail.com>
2022-12-26 20:33:32 +0200

    add function startNewGame

--- script.js
+++ script.js
@@ -3,10 +3,15 @@
 let secretNumber;
 let score;
 let highscore = 0;
-
-document.querySelector('.question').textContent = secretNumber; //to delet
-document.querySelector('.score').textContent = score;
-document.querySelector('.number-input').value = '';
+//Функція ініциації нової гри
+function startNewGame() {
+  secretNumber = Math.round(Math.random() * 20 + 1);
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number-input').value = '';
+  document.querySelector('.check').disabled = false;
+  document.querySelector('.question').textContent = secretNumber; //to delet
+}
 //Функція змінює очки та дає підказки
 function checkNumber(str, key = true) {
   document.querySelector('.guess-message').textContent = str;
@@ -14,13 +19,15 @@ function checkNumber(str, key = true) {
     score--;
     document.querySelector('.score').textContent = score;
   } else {
+    document.querySelector('.check').disabled = true;
+    document.querySelector('.question').textContent = secretNumber;
     if (highscore < score) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
   }
 }
-
+//Обробка кнопки "Проверить"
 document.querySelector('.check').addEventListener('click', function () {
   const guessingNumber = Number(document.querySelector('.number-input').value);
 
@@ -29,12 +36,19 @@ document.querySelector('.check').addEventListener('click', function () {
       'Введите число от 1 до 20!';
   } else if (guessingNumber === secretNumber) {
     checkNumber('Победа!', false);
-  } else if (guessingNumber > secretNumber) {
+  } else if (guessingNumber > secretNumber && score > 1) {
     checkNumber('Меньше!');
-  } else if (guessingNumber < secretNumber) {
+  } else if (guessingNumber < secretNumber && score > 1) {
     checkNumber('Больше!');
+  } else {
+    document.querySelector('.check').disabled = true;
+    checkNumber('Вы проиграли.');
   }
 });
+//Обробка кнопки "Сначала"
+document.querySelector('.again').addEventListener('click', startNewGame);
+
+startNewGame();
 /*1.Визначити число від 1 до 20
 2.Порівняти з введенним:
  а) якщо замале вивести - "Більше!",
