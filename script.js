0471bb3b601a35e33c325c6f82d70323b7656ba1
igostamikh <69596045+igostamikh@users.noreply.github.com>
2022-12-27 20:01:37 +0200

    update script.js

--- script.js
+++ script.js
@@ -1,52 +1,59 @@
-'use strict';
+"use strict";
 
 let secretNumber;
 let score;
 let highscore = 0;
 // Функція ініциації нової гри
 function startNewGame() {
-  secretNumber = Math.round(Math.random() * 20 + 1);
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number-input').value = '';
-  document.querySelector('.check').disabled = false;
-  document.querySelector('.question').textContent = secretNumber; // to delet
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number-input").value = "";
+  document.querySelector(".check").disabled = false;
+  document.querySelector(".question").textContent = secretNumber; // to delet
 }
 // Функція змінює очки та дає підказки
 function checkNumber(str, key = true) {
-  document.querySelector('.guess-message').textContent = str;
-  if (key) {
-    score -= 1;
-    document.querySelector('.score').textContent = score;
-  } else {
-    document.querySelector('.check').disabled = true;
-    document.querySelector('.question').textContent = secretNumber;
+  document.querySelector(".guess-message").textContent = str;
+  if (str === "Победа!") {
+    document.querySelector(".check").disabled = true;
+    document.querySelector(".question").textContent = secretNumber;
+    document.querySelector(".guess-message").style.color = "lime";
+    document.querySelector(".question").style.width = "50rem";
+    document.querySelector(".question").style.background = "lime";
+    document.querySelector(".question").style.color = "#fff";
     if (highscore < score) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
+    } else if (str === "Вы проиграли.") {
+      score -= 1;
+      document.querySelector(".score").textContent = score;
+      document.querySelector(".check").disabled = true;
+    } else {
+      score -= 1;
+      document.querySelector(".score").textContent = score;
     }
   }
 }
 // Обробка кнопки "Проверить"
-document.querySelector('.check').addEventListener('click', () => {
-  const guessingNumber = Number(document.querySelector('.number-input').value);
+document.querySelector(".check").addEventListener("click", () => {
+  const guessingNumber = Number(document.querySelector(".number-input").value);
 
   if (!guessingNumber || guessingNumber <= 0 || guessingNumber > 20) {
     // eslint-disable-next-line operator-linebreak
-    document.querySelector('.guess-message').textContent =
-      'Введите число от 1 до 20!';
+    document.querySelector(".guess-message").textContent =
+      "Введите число от 1 до 20!";
   } else if (guessingNumber === secretNumber) {
-    checkNumber('Победа!', false);
+    checkNumber("Победа!", false);
   } else if (guessingNumber > secretNumber && score > 1) {
-    checkNumber('Меньше!');
+    checkNumber("Меньше!");
   } else if (guessingNumber < secretNumber && score > 1) {
-    checkNumber('Больше!');
+    checkNumber("Больше!");
   } else {
-    document.querySelector('.check').disabled = true;
-    checkNumber('Вы проиграли.');
+    checkNumber("Вы проиграли.");
   }
 });
 // Обробка кнопки "Сначала"
-document.querySelector('.again').addEventListener('click', startNewGame);
+document.querySelector(".again").addEventListener("click", startNewGame);
 
 startNewGame();
