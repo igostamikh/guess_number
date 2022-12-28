20f9fd05f0a13d972138e524de88961f1be82d0f
igostamikh <69596045+igostamikh@users.noreply.github.com>
2022-12-28 17:10:40 +0200

    finished script.js

--- src/script.js
+++ src/script.js
@@ -1,67 +1,58 @@
-let secretNumber;
-let score;
+"use strict";
+
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
+let score = 20;
 let highscore = 0;
 // Функція ініциації нової гри
 function startNewGame() {
   secretNumber = Math.trunc(Math.random() * 20 + 1);
   score = 20;
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.number-input').value = '';
-  document.querySelector('.check').disabled = false;
-  document.querySelector('.guess-message').style.color = '#fff';
-  document.querySelector('.question').style.width = '25rem';
-  document.querySelector('.question').style.background = '#fff';
-  document.querySelector('.question').style.color = '#000';
-  document.querySelector('.question').textContent = '???'; // to delet
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number-input").value = "";
+  document.querySelector(".check").disabled = false;
+  document.querySelector(".guess-message").style.color = "#fff";
+  document.querySelector(".question").style.width = "25rem";
+  document.querySelector(".question").style.background = "#fff";
+  document.querySelector(".question").style.color = "#000";
+  document.querySelector(".question").textContent = "???";
+  document.querySelector(".guess-message").textContent =
+    "Введите число от 1 до 20!";
+  console.log(secretNumber);
 }
+console.log(secretNumber);
 // Функція змінює очки та дає підказки
-// eslint-disable-next-line no-unused-vars
-function checkNumber(str, key = true) {
-  document.querySelector('.guess-message').textContent = str;
-  if (str === 'Победа!') {
-    document.querySelector('.check').disabled = true;
-    document.querySelector('.question').textContent = secretNumber;
-    document.querySelector('.guess-message').style.color = 'lime';
-    document.querySelector('.question').style.width = '50rem';
-    document.querySelector('.question').style.background = 'lime';
-    document.querySelector('.question').style.color = '#fff';
-    if (highscore < score) {
-      highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
-    }
-  } else if (str === 'Вы проиграли.') {
+function checkNumber(str, color) {
+  document.querySelector(".guess-message").textContent = str;
+  if (!color) {
     score -= 1;
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.check').disabled = true;
-    document.querySelector('.guess-message').style.color = 'red';
-    document.querySelector('.question').style.width = '50rem';
-    document.querySelector('.question').style.background = 'red';
-    document.querySelector('.question').style.color = '#fff';
+    document.querySelector(".score").textContent = score;
   } else {
-    score -= 1;
-    document.querySelector('.score').textContent = score;
+    document.querySelector(".check").disabled = true;
+    document.querySelector(".question").textContent = secretNumber;
+    document.querySelector(".guess-message").style.color = color;
+    document.querySelector(".question").style.width = "50rem";
+    document.querySelector(".question").style.background = color;
+    document.querySelector(".question").style.color = "#fff";
+
+    if (highscore < score && color === "lime") {
+      highscore = score;
+      document.querySelector(".highscore").textContent = highscore;
+    }
   }
 }
-
 // Обробка кнопки "Проверить"
-document.querySelector('.check').addEventListener('click', () => {
-  const guessingNumber = Number(document.querySelector('.number-input').value);
-
+document.querySelector(".check").addEventListener("click", () => {
+  const guessingNumber = Number(document.querySelector(".number-input").value);
   if (!guessingNumber || guessingNumber <= 0 || guessingNumber > 20) {
-    // eslint-disable-next-line operator-linebreak
-    document.querySelector('.guess-message').textContent =
-      'Введите число от 1 до 20!';
+    document.querySelector(".guess-message").textContent =
+      "Введите число от 1 до 20!";
   } else if (guessingNumber === secretNumber) {
-    checkNumber('Победа!', false);
-  } else if (guessingNumber > secretNumber && score > 1) {
-    checkNumber('Меньше!');
-  } else if (guessingNumber < secretNumber && score > 1) {
-    checkNumber('Больше!');
+    checkNumber("Победа!", "lime");
+  } else if (score > 16) {
+    checkNumber(guessingNumber > secretNumber ? "Меньше!" : "Больше!");
   } else {
-    checkNumber('Вы проиграли.');
+    checkNumber("Вы проиграли.", "red");
   }
 });
 // Обробка кнопки "Сначала"
-document.querySelector('.again').addEventListener('click', startNewGame);
-
-startNewGame();
+document.querySelector(".again").addEventListener("click", startNewGame);
